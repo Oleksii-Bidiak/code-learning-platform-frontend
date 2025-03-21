46050b4e543b59792540d1b182446dc4e5c4c03c
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2025-03-21 12:26:00 +0200

    docker & github actions first setup

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname
+});
+
+const eslintConfig = [
+	...compat.config({
+		extends: [
+			'prettier',
+			'next/core-web-vitals',
+			'next/typescript',
+			'eslint-config-prettier',
+			'plugin:react-hooks/recommended',
+			'plugin:eslint-plugin-prettier/recommended'
+		],
+		rules: {
+			'prettier/prettier': 0,
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-empty-object-type': 'off'
+		}
+	})
+];
+
+export default eslintConfig;
